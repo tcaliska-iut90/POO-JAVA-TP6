8d8806446514f31372fec44cf5b3eac2a541c4a4
tcaliska-iut90 <turker.caliskan@edu.univ-fcomte.fr>
2024-03-12 17:06:51 +0100

    Class Personne

---
+++ Salarie.java
@@ -0,0 +1,19 @@
+public class Salarie extends Personne{
+    private String employeur;
+    private String numeroSecu;
+
+    public Salarie(){
+        super();
+    }
+    public Salarie(Salarie s){
+        super(s);
+        this.employeur = s.employeur;
+        this.numeroSecu = s.numeroSecu;
+    }
+
+    public Salarie(String unNom, int unAge, String unNumeroSecu, String unEmployeur){
+        super(unNom, unAge);
+        this.employeur = unEmployeur;
+        this.numeroSecu = unNumeroSecu;
+    }
+}
