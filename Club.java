ab7967054f99b93d9191a9ddc75e02ec53f15871
tcaliska-iut90 <turker.caliskan@edu.univ-fcomte.fr>
2024-03-12 17:52:41 +0100

    Class Club

---
+++ Club.java
@@ -0,0 +1,13 @@
+public class Club {
+    private String nomClub;
+    private String[] adherents;
+    public Club(){}
+    public Club(Club c){
+        this.nomClub = c.nomClub;
+        this.adherents = c.adherents;
+    }
+    public Club(String unNomClub, Personne[] desAdherents){
+        this.nomClub = unNomClub;
+        this.adherents = desAdherents;
+    }
+}
